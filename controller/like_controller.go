c24ac29d7f156ef6ae223788e678231ef8891289
glennprays <glennsagala@gmail.com>
2023-07-16 00:43:01 +0700

    bugfix: convert param into int64

--- controller/like_controller.go
+++ controller/like_controller.go
@@ -3,6 +3,7 @@ package controller
 import (
 	"log"
 	"net/http"
+	"strconv"
 	"twitter-clone-api/config/database"
 	"twitter-clone-api/middleware"
 
@@ -24,18 +25,29 @@ func LikeTweet(c *gin.Context) {
 
 	username, _, _ := middleware.GetUsernameAndRoleFromCookie(c)
 	tweetID := c.Param("tweetID")
+	tweetIDInt, err := strconv.ParseInt(tweetID, 10, 64)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	// tweetID := c.Param("tweetID")
+	// tweetID := 32
 
+	// query := `
+	// MATCH (u:User { username: $username })
+	// MATCH (t:Tweet)
+	// WHERE id(t) = $tweetID
+	// CREATE (u)-[:LIKES { timestamp: datetime() }]->(t)
+	// return id(t)
+	// `
 	query := `
-		MATCH (u:User { username: $username })
-		MATCH (t:Tweet)
+		MATCH (u:User { username: $username }), (t:Tweet)
 		WHERE id(t) = $tweetID
 		CREATE (u)-[:LIKES { timestamp: datetime() }]->(t)
-		return id(t)
 	`
 
 	result, err := session.Run(c, query, map[string]interface{}{
 		"username": username,
-		"tweetID":  tweetID,
+		"tweetID":  tweetIDInt,
 	})
 	if err != nil {
 		log.Println(err)
@@ -44,10 +56,10 @@ func LikeTweet(c *gin.Context) {
 	}
 
 	// Check if the like was successfully created
-	if result.Err() != nil {
+	if result.Err() == nil {
+		log.Println(result.Err())
 		c.JSON(http.StatusOK, gin.H{"message": "Tweet liked"})
 	} else {
-		log.Println(result.Err())
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to like tweet"})
 	}
 }
