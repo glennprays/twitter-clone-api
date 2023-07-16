c49d4d1121108c5bbfbc2060c7d570dcb1b0d87d
glennprays <glennsagala@gmail.com>
2023-07-17 04:54:17 +0700

    fix project

--- controller/user_controller.go
+++ controller/user_controller.go
@@ -3,6 +3,7 @@ package controller
 import (
 	"log"
 	"net/http"
+	"time"
 	"twitter-clone-api/config/database"
 	"twitter-clone-api/middleware"
 	"twitter-clone-api/models"
@@ -73,4 +74,110 @@ func WhoAmI(c *gin.Context) {
 		Role:     role,
 	}
 	c.JSON(http.StatusOK, responseData)
+}
+
+func GetUserHandler(c *gin.Context) {
+	driver, err := database.ConnectDB()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	defer driver.Close(c)
+
+	session := driver.NewSession(c, neo4j.SessionConfig{})
+	defer session.Close(c)
+
+	users, err := getUser(session, c)
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
+		return
+	}
+
+	c.JSON(http.StatusOK, users)
+
+}
+
+func getUser(session neo4j.SessionWithContext, c *gin.Context) ([]models.User, error) {
+
+	result, err := session.Run(c, "MATCH (p:User) RETURN p.name AS name, p.email AS email, p.username AS username, p.created AS created, p.password AS password", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var users []models.User
+
+	for result.Next(c) {
+		record := result.Record()
+
+		nameVal, nameExists := record.Get("name")
+		emailVal, emailExists := record.Get("email")
+		usernameVal, usernameExists := record.Get("username")
+		createdVal, createdExists := record.Get("created")
+
+		if !(nameExists || emailExists || usernameExists || createdExists) {
+			continue
+		}
+
+		name, _ := nameVal.(string)
+		email, _ := emailVal.(string)
+		username, _ := usernameVal.(string)
+		created, _ := createdVal.(time.Time)
+
+		user := models.User{
+			Name:     &name,
+			Email:    &email,
+			Username: username,
+			Created:  &created,
+		}
+		users = append(users, user)
+	}
+
+	return users, nil
+}
+
+func CreateUserHandler(c *gin.Context) {
+	driver, err := database.ConnectDB()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	defer driver.Close(c)
+
+	session := driver.NewSession(c, neo4j.SessionConfig{})
+	defer session.Close(c)
+
+	var user models.User
+
+	err = c.ShouldBindJSON(&user)
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Bad Request"})
+		return
+	}
+
+	err = createUser(c, session, user)
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"message": "User created successfully"})
+}
+
+// // createUser creates a new user node in the Neo4j database.
+func createUser(c *gin.Context, session neo4j.SessionWithContext, user models.User) error {
+
+	_, err := session.Run(c, "CREATE (u:User {name: $name, username: $username, email: $email, password: $password, created: datetime()})", map[string]interface{}{
+		"name":     user.Name,
+		"username": user.Username,
+		"email":    user.Email,
+		"password": user.Password,
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
